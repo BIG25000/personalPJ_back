cb5e72e53a88b06ff6122d7179500303ac4c1d8b
BIG25000~ <pongsatornbpst@gmail.com>
2024-02-15 19:00:30 +0700

    login register not get token

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -8,16 +8,14 @@ exports.register = catchError(async (req, res, next) => {
   const existsUser = await userService.findUserByEmail(req.body.email);
 
   if (existsUser) {
-    catchError("email already in user", 400);
+    createError("EMAIL_IN_USER", 400);
   }
 
   req.body.password = await hashService.hash(req.body.password);
   delete req.body.confirmPassword;
-
   const newUser = await userService.createUser(req.body);
   const payload = { userId: newUser.id };
   const accessToken = jwtService.sign(payload);
-
   delete newUser.password;
 
   res.status(200).json({ accessToken, newUser });
