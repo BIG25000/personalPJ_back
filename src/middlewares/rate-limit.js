e20689d767e979508f70ba3629a48f8d50268b14
BIG25000~ <pongsatornbpst@gmail.com>
2024-02-24 19:05:59 +0700

    editProject

--- src/middlewares/rate-limit.js
+++ src/middlewares/rate-limit.js
@@ -2,6 +2,6 @@ const { rateLimit } = require("express-rate-limit");
 
 module.exports = rateLimit({
   windowMs: 1000 * 60 * 5,
-  limit: 1000,
+  limit: 100000000,
   message: { message: "too many request in a given period" },
 });
